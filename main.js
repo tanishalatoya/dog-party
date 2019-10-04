c3e52ea5f5e9d05d6a24f908f31846205d633ccf
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-10-04 16:35:27 -0600

    Target html elements with variables

---
+++ main.js
@@ -0,0 +1,16 @@
+/*Target all the elements that you want to manipulate using a variable to store the querySelector of that element
+
+*Items that I want to target:
+input
+button
+h1
+
+Write a function to replace the text in the h1 with the user's
+input from the input box
+
+Create an event listener to execute the function once the
+"event" happens*/
+
+var someDogs = document.querySelector('#dog-name');
+var input = document.querySelector('input');
+var button = document.querySelector('button');
