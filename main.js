78d316e5ffbe5bcdc47d08b6ed2b714b2c0500f4
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-10-06 09:06:30 -0600

    Remove console.logchecks

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ var someDog = document.querySelector('#dog-name');
 var input = document.querySelector('#name');
 var button = document.querySelector('#button');
 
-console.log(someDog);
-console.log(input);
-console.log(button);
-
 button.addEventListener('click', replaceText);
 
 function replaceText() {
