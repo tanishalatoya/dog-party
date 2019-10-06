6106ab4fb9976202aaa2204b645862d131ae9a2a
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-10-06 14:41:20 -0600

    Create querySelectors for paragraphs in main

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ function replaceText() {
   someDog.innerText = input.value;
   input.value = '';
 }
+
+/* For third iteration, target
+3 buttons in main
+3 paragraphs in main
+
+Add event listeners to 3 butttons
+
+A class??? to paragraphs to hide them???
+*/
+
+var paragraph1 = document.querySelector('#paragraph1');
+var paragraph2 = document.querySelector('#paragraph2');
+var paragrah3 = document.querySelector('#paragrah3');
+
+console.log(paragrah1);
+console.log(paragrah2);
+console.log(paragrah3);
