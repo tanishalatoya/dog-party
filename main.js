42221531ec0004ccda8f8dd8610113f7b24fa01e
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-10-06 15:56:00 -0600

    Remove whitespace from script

--- main.js
+++ main.js
@@ -10,22 +10,21 @@ function replaceText() {
   input.value = '';
 }
 
-/* For third iteration, target
-3 buttons in main
-3 paragraphs in main
-
-Add event listeners to 3 butttons
-
-A class??? to paragraphs to hide them???
-*/
-
 var paragraph1 = document.querySelector('#paragraph1');
 var paragraph2 = document.querySelector('#paragraph2');
-var paragrah3 = document.querySelector('#paragrah3');
+var paragraph3 = document.querySelector('#paragraph3');
 var how = document.querySelector('#how');
 var what = document.querySelector('#what');
 var facts = document.querySelector('#facts');
 
-how.addEventListener('click', );
-what.addEventListener('click',);
-facts.addEventListener('click', );
+how.addEventListener('click', function() {
+  paragraph1.classList.toggle('hidden');
+});
+
+what.addEventListener('click', function(){
+  paragraph2.classList.toggle('hidden');
+});
+
+facts.addEventListener('click', function() {
+  paragraph3.classList.toggle('hidden');
+});
