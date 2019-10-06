51cd9c029a8b75acbfd1d582ee09541bc56f98df
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-10-06 09:03:53 -0600

    Remove comments

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-/*Target all the elements that you want to manipulate using a variable to store the querySelector of that element
-
-*Items that I want to target:
-input
-button
-h1
-
-Write a function to replace the text in the h1 with the user's
-input from the input box
-
-Create an event listener to execute the function once the
-"event" happens*/
 
 var someDog = document.querySelector('#dog-name');
-var input = document.querySelector('input');
-var button = document.querySelector('button');
+var input = document.querySelector('#name');
+var button = document.querySelector('#button');
+
+console.log(someDog);
+console.log(input);
+console.log(button);
 
 button.addEventListener('click', replaceText);
 
